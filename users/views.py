f67b0f0c41ff88cc00d344bddfa66db222d85a45
Christine Mumbi <christinemumbi286@gmail.com>
2026-06-26 17:09:53 +0300

    Add user authentication and profile management

---
+++ users/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib import messages
+from .forms import RegisterForm
+from django.contrib.auth import logout
+
+# Create your views here.
+def register(request):
+    if request.method=="POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Welcome {username}, your account has been successfully created!')
+            return redirect('login')
+    form = RegisterForm()
+    return render(request,'users/register.html',{'form':form})
+
+def logout_view(request):
+    logout(request)
+    return render(request,'users/logout.html')
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
+
+
+
